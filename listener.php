19433a03c7f264389413ebda9217cf5b62f06a7a
Aleksandr Tochilin <alex8079@gmail.com>
2024-08-11 01:09:11 +0300

    cleanup

--- listener.php
+++ listener.php
@@ -2,8 +2,8 @@
 
 use Core\Helper\ExceptionHandler;
 
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// error_reporting(E_ALL);
 
 const ROOT = __DIR__;
 
